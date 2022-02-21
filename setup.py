5be35d52c0d6672002e0b1beb7cced0319b66b38
John Meinken <meinkejf@ucmail.uc.edu>
2022-02-21 13:19:57 -0500

    fixed mistake in requirements on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="redcap_importer",
-    version="1.0.2",
+    version="1.0.3",
     description="Redcap Importer tool source",
     url="https://github.com/Center-for-Health-Informatics/redcap_importer",
     author="John Meinken",
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "Django >= 2.0, < 4.0",
+        "Django >= 2.0",
         "django-crispy-forms",
         "requests",
         "python-dateutil",
